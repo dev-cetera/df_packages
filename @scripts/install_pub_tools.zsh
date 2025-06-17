473bafef55a5ee90f2a055ec4908c6910dcc8988
robmllze <86869208+robmllze@users.noreply.github.com>
2025-06-17 03:56:08 -0400

    update

--- @scripts/install_pub_tools.zsh
+++ @scripts/install_pub_tools.zsh
@@ -5,6 +5,7 @@ dart pub global deactivate df_generate_header_comments; dart pub global activate
 dart pub global deactivate df_generate_localization; dart pub global activate df_localization
 dart pub global deactivate df_generate_screen; dart pub global activate df_generate_screen
 dart pub global deactivate df_bulk_replace; dart pub global activate df_bulk_replace
+dart pub global deactivate df_combine_files; dart pub global activate df_combine_files
 
 # Other tools.
 dart pub global deactivate flutterfire_cli; dart pub global activate flutterfire_cli
